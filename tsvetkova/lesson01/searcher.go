f2e2528aa15a2a09f862171d711e3382f15c980c
Elena Tsvetkova <kaello.skye@gmail.com>
2020-08-24 09:59:39 +0300

    Add goroutines

--- tsvetkova/lesson01/searcher.go
+++ tsvetkova/lesson01/searcher.go
@@ -7,50 +7,90 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 )
 
-func Search(query string, pages []string) ([]string, error) {
-	var result []string
-	for _, url := range pages {
-		response, err := http.Get(url)
-		if err != nil {
-			return result, err
-		}
-		defer response.Body.Close()
+func getPage(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
 
-		b, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return result, err
-		}
-
-		if strings.Contains(string(b), query) {
-			result = append(result, url)
-		}
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
 	}
 
-	return result, nil
+	return string(b), nil
+}
+
+func Search(query string, pages []string) []string {
+	var wg sync.WaitGroup
+	var result []string
+
+	for _, url := range pages {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+
+			text, err := getPage(url)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			if strings.Contains(text, query) {
+				result = append(result, url)
+			}
+		}(url)
+	}
+
+	wg.Wait()
+	return result
 }
 
 func parseArgs() (string, []string) {
-	query := flag.String("query", "go version", "search query")
-	pagesString := flag.String("pages", "https://golang.org/pkg/math/,https://golang.org/doc/install,https://stackoverflow.com/questions/42952979/go-version-command-shows-old-version-number-after-update-to-1-8", "pages to search")
+	query := flag.String("query", "файл", "search query")
+	pagesString := flag.String("pages", "", "pages to search")
 	flag.Parse()
 
-	pages := strings.Split(*pagesString, ",")
+	var pages []string
+	if len(*pagesString) == 0 {
+		pages = []string{
+			"https://geekbrains.ru/posts/perspektivy-java-ai-i-tvorcheskie-podrostki",					// false
+			"https://geekbrains.ru/posts/kak-podklyuchat-fajly-php-i-zachem-ehto-voobshche-nuzhno",		// true
+			"https://geekbrains.ru/posts/data_recovery",												// true
+			"https://geekbrains.ru/posts/kurs-dlya-detej-po-razrabotke-igr-na-python",					// false
+			"https://geekbrains.ru/posts/tts_python",													// true
+			"https://geekbrains.ru/posts/postroenie-nebolshogo-centra-sertifikacii-na-osnove-openssl",	// true
+			"https://geekbrains.ru/posts/programming_types",											// false
+			"https://geekbrains.ru/posts/npp_java",														// true
+			"https://geekbrains.ru/posts/100-goryachih-klavish-photoshop-dlya-ognennogo-rezultata",		// true
+			"https://geekbrains.ru/posts/brosat-razvitie-sobstvennyh-produktov-ya-ne-sobiralsya",		// false
+			"https://geekbrains.ru/posts/chto-takoe-css-obyasnyaem-prostymi-slovami",					// true
+			"https://geekbrains.ru/posts/kak-rabotat-iz-doma-bolee-ehffektivno",						// false
+			"https://geekbrains.ru/posts/kak-zashifrovat-pochtu-i-sdelat-ehlektronnuyu-podpis",			// true
+			"https://geekbrains.ru/posts/kak-za-pyat-shagov-nastroit-skvoznuyu-analitiku",				// false
+			"https://geekbrains.ru/posts/9-horoshih-servisov-proverki-koda-dlya-razrabotchikov",		// false
+		}
+	} else {
+		pages = strings.Split(*pagesString, ",")
+	}
+
 	return *query, pages
 }
 
 func main() {
 	query, pages := parseArgs()
-	fmt.Printf("Searching for: %q on %d pages\n", query, len(pages))
-	result, err := Search(query, pages)
-	if err != nil {
-		log.Fatalf("Error occured: %s", err)
-	}
+	fmt.Printf("Searching for %q on %d pages\n", query, len(pages))
+
+	result := Search(query, pages)
+
 	if len(result) == 0 {
 		fmt.Println("No matches found\n")
 	} else {
-		fmt.Printf("Found match on %d pages\n", len(result))
+		fmt.Printf("Found match on %d page(s)\n", len(result))
 		for idx, url := range result {
 			fmt.Printf("%d. %s\n", idx+1, url)
 		}
