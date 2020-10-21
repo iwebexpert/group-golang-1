127c4db469d4e04d84a045784abaf1d45ed26256
Petr Ustyuzhanin <petrakliy@gmail.com>
2020-10-22 01:37:43 +0300

    syntax fixes with acces to db

--- petr-ustyuznhanin/hw-4/examples/taskManagerWeb/models/tasks.go
+++ petr-ustyuznhanin/hw-4/examples/taskManagerWeb/models/tasks.go
@@ -14,7 +14,7 @@ type TaskItemSlice []TaskItem
 
 //GetAllTasks и последующие методы под капотом у ORM
 func GetAllTasks(db *sql.DB) (TaskItemSlice, error) {
-	row, err := db.Query("SELECT ID, Text, Completed FROM TaksItems")
+	row, err := db.Query("SELECT ID, Text, Completed FROM TaskItems")
 	if err != nil {
 		return nil, err
 	}
@@ -41,7 +41,7 @@ func (task *TaskItem) Insert(db *sql.DB) error {
 
 //Update под капотом у ORM
 func (task *TaskItem) Update(db *sql.DB) error {
-	_, err := db.Exec("UPDATE TaskItems SET Text = ?, Completed = ?) WHERE ID = ?",
+	_, err := db.Exec("UPDATE TaskItems SET Text = ?, Completed = ? WHERE ID = ?",
 		task.Text, task.Completed, task.ID)
 
 	return err
