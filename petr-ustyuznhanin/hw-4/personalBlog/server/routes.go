fcf9c3a8c80acad28b6dfa840a1acc98fac8ad71
Petr Ustyuzhanin <petrakliy@gmail.com>
2020-10-31 19:58:34 +0300

    add handlers and import trouble

--- petr-ustyuznhanin/hw-4/personalBlog/server/routes.go
+++ petr-ustyuznhanin/hw-4/personalBlog/server/routes.go
@@ -6,6 +6,11 @@ import (
 
 func (serv *Server) bindRoutes(r *chi.Mux) {
 	r.Route("/", func(r chi.Router) {
-		//..
+		r.Get("/{template}", serv.getTemplateHandler)
+		r.Route("api/v1", func(r chi.Router) {
+			r.Post("/posts", serv.postPostHandler)
+			r.Delete("/posts/{id}", serv.deletePostHandler)
+			r.Put("/tasks/{id}", serv.putTaskHandler)
+		})
 	})
 }
--- petr-ustyuznhanin/hw-4/personalBlog/server/server.go
+++ petr-ustyuznhanin/hw-4/personalBlog/server/server.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
+	"server/models"
 
 	"github.com/go-chi/chi"
 
@@ -38,4 +40,22 @@ func (serv *Server) Start(addr string) error {
 	serv.bindRoutes(r)
 	serv.lg.Debug("server is started...")
 	return http.ListenAndServe(addr, r)
+}
+
+func (serv *Server) SendError(w http.ResponseWriter, err error, code int, obj ...interface{}) {
+	serv.lg.WithField("data", obj).WithError(err).Error("server error")
+	w.WriteHeader(code)
+	errModel := models.ErrorModel{
+		Code:     code,
+		Err:      err.Error(),
+		Desc:     "server error",
+		Internal: obj,
+	}
+
+	data, _ := json.Marshal(errModel)
+	w.Write(data)
+}
+
+func (serv *Server) SendInternalError(w http.ResponseWriter, err error, obj ...interface{}) {
+	serv.SendError(w, err, http.StatusInternalServerError, obj)
 }
