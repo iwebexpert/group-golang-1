dea4412b0284825a9f2ee73a9c4a0755ded22fa9
Petr Ustyuzhanin <petrakliy@gmail.com>
2020-10-31 21:37:11 +0300

    change go mod

---
+++ petr-ustyuznhanin/hw-4/personalBlog/go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.14
+
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/sirupsen/logrus v1.7.0
+)
---
+++ petr-ustyuznhanin/hw-4/personalBlog/server/handlers.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"server/models"
+
+	"github.com/go-chi/chi"
+)
+
+func (serv *Server) getTemplateHandler(w http.ResponseWriter, r *http.Request) {
+	templateName := chi.URLParam(r, "template")
+
+	if templateName == "" {
+		templateName = serv.indexTemplate
+	}
+
+	file, err := os.Open(path.Join(serv.rootDir, serv.templatesDir, templateName))
+	if err != nil {
+		if err != os.ErrNotExist {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		serv.SendInternalError(w, err)
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		serv.SendInternalError(w, err)
+		return
+	}
+
+	templ, err := template.New("Page").Parse(string(data))
+	if err != nil {
+		serv.SendInternalError(w, err)
+		return
+	}
+
+	// to be continued
+}
+
+func (serv *Server) postPostHandler(w http.ResponseWriter, r *http.Request) {
+	data, _ := ioutil.ReadAll(r.Body)
+
+	post := models.PostItem{}
+}
